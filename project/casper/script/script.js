dfa763d6d7fa4eeaecab527586d4348153fedd10
kimnani01 <165613415+kimnani01@users.noreply.github.com>
2024-06-11 11:52:05 +0900

    모바일 js, jq

--- project/casper/script/script.js
+++ project/casper/script/script.js
@@ -36,3 +36,44 @@ nav.addEventListener('mouseout',()=>{
         i.style.height = '0';
     }
 })
+
+//====================모바일 nav js
+//m_nav 상단 변수 존재함
+//1. 모바일 전체메뉴 아이콘(m_nav > all_nav) 클릭 시
+//1-1. 모바일 내비가(m_nav > nav) 보이는 상태라면 -> 숨기기
+//1-2. 모바일 내비가 숨겨진 상태라면 -> 보이기
+const all_nav_m = m_nav.querySelector('.all_nav')
+const nav_m = m_nav.querySelector('nav')
+let boolean = false;//안보이는 상태(기본)
+nav_m.classList.add('m_nav_hide')//숨긴값 클래스 적용
+console.log(all_nav_m, nav_m) //ok
+all_nav_m.addEventListener('click', (e)=>{
+    e.preventDefault(); //a  href 막기
+    //=============클래스(classList.toggle)
+    nav_m.classList.toggle('m_nav_hide')
+    //=============조건문(if) 
+    //boolean = !boolean
+    /* if(boolean == true){
+        nav_m.style.display = 'block'
+    }else{
+        nav_m.style.display = 'none'
+    } */
+    //삼항조건연산자 조건 ? 참 : 거짓결과;
+    //nav_m.style.display = boolean ? 'block' : 'none'
+})
+//2. 모바일 서브 메뉴 모두 숨기기(기본)
+//3. 모바일 주 메뉴 클릭 시
+//3-1. 모든 서브 메뉴 숨기기
+//3-2. 해당 주 메뉴의 서브 메뉴 보이기
+const sub_m = nav_m.querySelectorAll('.sub')
+const gnb_li_m = nav_m.querySelectorAll('.gnb > li')
+console.log(sub_m, gnb_li_m)
+for(i of sub_m){i.style.display = 'none'}
+for (let i of gnb_li_m){
+    console.log(i)
+    i.addEventListener('click',()=>{
+        console.log(i.children[1])
+        for(j of sub_m){j.style.display = 'none'}
+        i.children[1].style.display = 'block'
+    })
+}
